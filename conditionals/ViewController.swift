1449487955f996f652a3af61f9b43c7cf02ec322
Carlos Mugica <cmugica4@cloud.com>
2022-02-10 14:15:24 -0700

    balls

--- conditionals/ViewController.swift
+++ conditionals/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var x = 3  //this variable is for our if statments
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func whenpressed(_ sender: Any) {
+        if x == 3 {
+                print(x)
+            }
+        else if x == 4 {
+            print("x is equal to 4.")
+            
+        }
+else
+{
+    print("x is not equal to 3 or 4")
 
 }
-
+ switch x
+        {
+case 0:
+    print("x is equal to 0")
+ case 4:
+     print("x is equal to 4")
+ case 5:
+     print("x is equal to 5")
+default:
+    print("x is not any of the values")
+    
+    
+}
+    }
+}
